b0ad1129339483fe7e6822c4e12178fde3e5de70
zhihaop <zhihaop@qq.com>
2023-08-11 13:34:10 +0800

    feat: sync test

--- bench/bench_executor.cc
+++ bench/bench_executor.cc
@@ -1,8 +1,11 @@
+#include <pedrolib/concurrent/latch.h>
 #include <pedrolib/logger/logger.h>
 #include <pedronet/eventloop.h>
 #include <pedronet/selector/epoller.h>
+#include <pedronet/logger/logger.h>
 #include <future>
 
+using pedrolib::Latch;
 using pedrolib::Logger;
 using pedronet::EpollSelector;
 using pedronet::EventLoop;
@@ -12,17 +15,24 @@ int main() {
 
   Logger logger("bench");
   logger.SetLevel(Logger::Level::kTrace);
+  pedronet::logger::SetLevel(Logger::Level::kWarn);
   auto defer = std::async(std::launch::async, [&] { executor.Loop(); });
 
-  int n = 1e6 + 5;
+  int n = 1e7 + 5;
   std::atomic<int> counter = 0;
 
+  Latch latch(1);
   logger.Info("bench start");
   for (int i = 0; i < n; ++i) {
-    executor.Schedule([&] { counter++; });
+    executor.Schedule([&] {
+      counter++;
+      if (counter == n) {
+        latch.CountDown();
+      }
+    });
   }
-  logger.Info("bench end");
-
+  latch.Await();
+  logger.Info("bench end: {}", counter.load());
   executor.Close();
   return 0;
 }
